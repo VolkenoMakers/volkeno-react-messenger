18c786264e9075b68ce8507b6625e0a0b055c4c9
Mbaye Niass <mbayeniass19@gmail.com>
2024-04-24 16:08:42 +0000

    Updates

---
+++ precache-manifest.c5071c020094774522749bcbefa8147b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f60a3f8652c1eeadb7e3312a6768396",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac861b284a99887ab7ab",
+    "url": "./static/css/2.8fbde0f0.chunk.css"
+  },
+  {
+    "revision": "6c5a60916ee9d31ef0f6",
+    "url": "./static/css/main.cf3ab87b.chunk.css"
+  },
+  {
+    "revision": "ac861b284a99887ab7ab",
+    "url": "./static/js/2.2c06a7c0.chunk.js"
+  },
+  {
+    "revision": "2bf9f12c31d8ea8cd766ba3a4aafc65a",
+    "url": "./static/js/2.2c06a7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5a60916ee9d31ef0f6",
+    "url": "./static/js/main.64434187.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.64434187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b4ac7559e4e91f27dd",
+    "url": "./static/js/runtime-main.5bf4337d.js"
+  }
+]);
