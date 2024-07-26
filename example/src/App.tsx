a577f6385c1a64203735e121a74f3fc5218b95a2
Paul Gomis <gomispapaul6@gmail.com>
2024-07-26 22:07:23 +0000

    fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,4 @@
 // import axios from 'axios'
-import axios from 'axios'
 import React from 'react'
 import VolkenoReactMessenger from 'volkeno-react-messenger'
 import 'volkeno-react-messenger/dist/index.css'
@@ -8,62 +7,62 @@ import 'volkeno-react-messenger/dist/index.css'
 
 // yad
 // const token ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldkB2b2xrZW5vLmNvbSIsImV4cCI6MTk1NDMyMjM3OSwiZW1haWwiOiJkZXZAdm9sa2Vuby5jb20iLCJvcmlnX2lhdCI6MTY5NTEyMjM3OX0.hRkniFxpbFI33T8Df21zKKyDRoCIzzhwATsLKAGG0zk' /* provide the token */
-const token =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YxL2FwcC9lbi9sb2dpbiIsImlhdCI6MTcyMjAzMDE2NCwiZXhwIjoxNzIyMDMzNzY0LCJuYmYiOjE3MjIwMzAxNjQsImp0aSI6Ill5YlU5eFNDVjlGNHg3d2oiLCJzdWIiOiIxNzIyMDE2NDAxNzQwWkc3MDc0ODI1IiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.mDQbOLoHdxWRVWuCJyy0Xk9H3gcHSovWdQdZYE8Xx4s' /* provide the token */
+// const token =
+//   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YxL2FwcC9lbi9sb2dpbiIsImlhdCI6MTcyMjAzMDE2NCwiZXhwIjoxNzIyMDMzNzY0LCJuYmYiOjE3MjIwMzAxNjQsImp0aSI6Ill5YlU5eFNDVjlGNHg3d2oiLCJzdWIiOiIxNzIyMDE2NDAxNzQwWkc3MDc0ODI1IiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.mDQbOLoHdxWRVWuCJyy0Xk9H3gcHSovWdQdZYE8Xx4s' /* provide the token */
 
-// yad
-// const ApiBaseUrl = 'https://yaay-ak-doom-api.volkeno-engineering.click'
-const ApiBaseUrl = 'http://127.0.0.1:8000/api/v1/app/fr/'
+// // yad
+// // const ApiBaseUrl = 'https://yaay-ak-doom-api.volkeno-engineering.click'
+// const ApiBaseUrl = 'http://127.0.0.1:8000/api/v1/app/fr/'
 
-const config = {
-  headers: {
-    Authorization: `Bearer ${token}`
-  }
-}
+// const config = {
+//   headers: {
+//     Authorization: `Bearer ${token}`
+//   }
+// }
 const App = () => {
-  const [user, setUser] = React.useState<any>(null)
+  // const [user, setUser] = React.useState<any>(null)
 
-  React.useEffect(() => {
-    axios
-      // yad
-      // .get(ApiBaseUrl + '/api/auth/me', config)
-      .get(ApiBaseUrl + 'me', config)
-      .then((response) => {
-        const userData = response.data.data
-        setUser(userData)
-      })
-      .catch((error) => {
-        console.error('Error:', error)
-      })
-  }, [])
+  // React.useEffect(() => {
+  //   axios
+  //     // yad
+  //     // .get(ApiBaseUrl + '/api/auth/me', config)
+  //     .get(ApiBaseUrl + 'me', config)
+  //     .then((response) => {
+  //       const userData = response.data.data
+  //       setUser(userData)
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error)
+  //     })
+  // }, [])
 
   return (
     <VolkenoReactMessenger
-      // user={{}}
-      // token={''}
-      // apiBaseUrl={''}
-      // setApiPostEndpoint={''}
-      // setApiListUsersEndpoint={''}
-      // setApiConversationUserEndpoint={''}
+      user={{}}
+      token={''}
+      apiBaseUrl={''}
+      setApiPostEndpoint={''}
+      setApiListUsersEndpoint={''}
+      setApiConversationUserEndpoint={''}
 
-      user={user}
-      token={token}
-      apiBaseUrl={ApiBaseUrl}
-      // yad
-      // setApiPostEndpoint={'/api/messages/'}
-      setApiPostEndpoint={'messages'}
-      // yad
-      // setApiListUsersEndpoint={'/api/medecins/?limit=1000'}
-      setApiListUsersEndpoint={'students/'}
-      // yad
-      // setApiConversationUserEndpoint={`/api/user/${user?.slug}/conversations/`}
-      setApiConversationUserEndpoint={`conversations-by-user/${user?.user_id}`}
-      setStyle='dag'
-      isMultiList={false}
-      setFirstListLabel='Admin'
-      dataStructure='new'
-      // setSecondListLabel ='Medecins'
-      // setSecondListUsersEndpoint={'/api/medecins'}
+      // user={user}
+      // token={token}
+      // apiBaseUrl={ApiBaseUrl}
+      // // yad
+      // // setApiPostEndpoint={'/api/messages/'}
+      // setApiPostEndpoint={'messages'}
+      // // yad
+      // // setApiListUsersEndpoint={'/api/medecins/?limit=1000'}
+      // setApiListUsersEndpoint={'students/'}
+      // // yad
+      // // setApiConversationUserEndpoint={`/api/user/${user?.slug}/conversations/`}
+      // setApiConversationUserEndpoint={`conversations-by-user/${user?.user_id}`}
+      // setStyle='dag'
+      // isMultiList={false}
+      // setFirstListLabel='Admin'
+      // dataStructure='new'
+      // // setSecondListLabel ='Medecins'
+      // // setSecondListUsersEndpoint={'/api/medecins'}
     />
   )
 }
